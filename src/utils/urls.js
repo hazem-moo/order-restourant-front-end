f0082cb463ae27f343f7bfc65eb573d9b75949de
hezem-moo <zoma.9099@gmail.com>
2025-10-05 00:57:25 +0300

    edit .env

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 // const url = `http://localhost:1337/api`;
 // export const PropsUrl = `http://localhost:1337`;
-const url = `https://funny-harmony-ea89b56a43.strapiapp.com/api`;
+
+const url = process.env.NEXT_PUBLIC_STRAPI_URL;
 const API_TOCKEN = process.env.STRAPI_API_TOKEN;
 export const PropsUrl = `https://funny-harmony-ea89b56a43.strapiapp.com`;
 
